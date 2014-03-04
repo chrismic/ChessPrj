da90053da4068e1a8cb2b93c564ce7b0fe08b278
chrismic <chrismic@mail.gvsu.edu>
2014-03-03 21:55:57 -0500

    Mosly finished Bishop class
    Need to be looked over but seems mostly finished

--- src/chess/Bishop.java
+++ src/chess/Bishop.java
@@ -25,7 +25,123 @@ public class Bishop extends ChessPiece{
 	 *****************************************************************/
 	public boolean isValidMove(Move move, IChessPiece[][] board) {
 		if(super.isValidMove(move, board)) {
-
+			// if the move is a diagonal
+			if(Math.abs(move.toColumn-move.toRow)==Math.abs(move.fromColumn-move.fromRow)){
+				
+				// if the move is down
+				if(move.toRow>move.fromRow){
+					// if the move is down right
+					if(move.toColumn>move.fromColumn){
+						int count = 0;
+						int c = move.fromColumn+1;
+						int r = move.fromRow+1;
+						for(int i=move.fromRow;i<move.toRow;i++){
+							if(board[r][c].player()==Player.BLACK ||
+									board[r][c].player()==Player.WHITE){
+								count++;
+							}
+							else{
+								
+							}
+							c++;
+							r++;
+						}
+						if(count==0){
+							return true;
+						}
+						else{
+							return false;
+						}
+					}
+					// if the move is down left
+					else if(move.toColumn<move.fromColumn){
+						int count = 0;
+						int c = move.fromColumn-1;
+						int r = move.fromRow+1;
+						for(int i=move.fromRow;i<move.toRow;i++){
+							if(board[r][c].player()==Player.BLACK ||
+									board[r][c].player()==Player.WHITE){
+								count++;
+							}
+							else{
+								
+							}
+							c--;
+							r++;
+						}
+						if(count==0){
+							return true;
+						}
+						else{
+							return false;
+						}
+					}
+					else{
+						return false;
+					}
+				}
+				
+				// if the move is up
+				else if(move.toRow<move.fromRow){
+					
+					// if the move is up right
+					if(move.toColumn>move.fromColumn){
+						int count = 0;
+						int c = move.fromColumn+1;
+						int r = move.fromRow-1;
+						for(int i=move.fromRow;i>move.toRow;i--){
+							if(board[r][c].player()==Player.BLACK ||
+									board[r][c].player()==Player.WHITE){
+								count++;
+							}
+							else{
+								
+							}
+							c++;
+							r--;
+						}
+						if(count==0){
+							return true;
+						}
+						else{
+							return false;
+						}
+					}
+					
+					// if the move is up left
+					else if(move.toColumn<move.fromColumn){
+						int count = 0;
+						int c = move.fromColumn-1;
+						int r = move.fromRow-1;
+						for(int i=move.fromRow;i>move.toRow;i--){
+							if(board[r][c].player()==Player.BLACK ||
+									board[r][c].player()==Player.WHITE){
+								count++;
+							}
+							else{
+								
+							}
+							c--;
+							r--;
+						}
+						if(count==0){
+							return true;
+						}
+						else{
+							return false;
+						}
+					}
+					else{
+						return false;
+					}
+				}
+				else {
+					return false;
+				}
+			}
+			else{
+				return false;
+			}
 		}
 		else{
 			return false;
