19ac754666e6921a03f70ac6e7ac1c5a36a55e73
chrismic <chrismic@mail.gvsu.edu>
2014-03-03 22:07:17 -0500

    Mostly completed Queen class.
    basically a combined Rook and Bishop class that first checks if the move
    is diagonal and if not checks if its in the same row or same column

--- src/chess/Queen.java
+++ src/chess/Queen.java
@@ -24,7 +24,222 @@ public class Queen extends ChessPiece{
 	 *****************************************************************/
 	public boolean isValidMove(Move move, IChessPiece[][] board) {
 		if(super.isValidMove(move, board)) {
+			// if its diagonal
+			if(Math.abs(move.toColumn-move.toRow)==Math.abs(move.fromColumn-move.fromRow)){
 
+				// if the move is down
+				if(move.toRow>move.fromRow){
+					// if the move is down right
+					if(move.toColumn>move.fromColumn){
+						int count = 0;
+						int c = move.fromColumn+1;
+						int r = move.fromRow+1;
+						for(int i=move.fromRow;i<move.toRow;i++){
+							if(board[r][c].player()==Player.BLACK ||
+									board[r][c].player()==Player.WHITE){
+								count++;
+							}
+							else{
+
+							}
+							c++;
+							r++;
+						}
+						if(count==0){
+							return true;
+						}
+						else{
+							return false;
+						}
+					}
+					// if the move is down left
+					else if(move.toColumn<move.fromColumn){
+						int count = 0;
+						int c = move.fromColumn-1;
+						int r = move.fromRow+1;
+						for(int i=move.fromRow;i<move.toRow;i++){
+							if(board[r][c].player()==Player.BLACK ||
+									board[r][c].player()==Player.WHITE){
+								count++;
+							}
+							else{
+
+							}
+							c--;
+							r++;
+						}
+						if(count==0){
+							return true;
+						}
+						else{
+							return false;
+						}
+					}
+					else{
+						return false;
+					}
+				}
+
+				// if the move is up
+				else if(move.toRow<move.fromRow){
+
+					// if the move is up right
+					if(move.toColumn>move.fromColumn){
+						int count = 0;
+						int c = move.fromColumn+1;
+						int r = move.fromRow-1;
+						for(int i=move.fromRow;i>move.toRow;i--){
+							if(board[r][c].player()==Player.BLACK ||
+									board[r][c].player()==Player.WHITE){
+								count++;
+							}
+							else{
+
+							}
+							c++;
+							r--;
+						}
+						if(count==0){
+							return true;
+						}
+						else{
+							return false;
+						}
+					}
+
+					// if the move is up left
+					else if(move.toColumn<move.fromColumn){
+						int count = 0;
+						int c = move.fromColumn-1;
+						int r = move.fromRow-1;
+						for(int i=move.fromRow;i>move.toRow;i--){
+							if(board[r][c].player()==Player.BLACK ||
+									board[r][c].player()==Player.WHITE){
+								count++;
+							}
+							else{
+
+							}
+							c--;
+							r--;
+						}
+						if(count==0){
+							return true;
+						}
+						else{
+							return false;
+						}
+					}
+					else{
+						return false;
+					}
+				}
+				else {
+					return false;
+				}
+			}
+			// if the selected move is in the same row
+			if(move.fromRow==move.toRow){
+				
+				// if the player wants to move left
+				if(move.toColumn<move.fromColumn){
+					int count = 0;
+					for(int i=move.fromColumn-1;i>move.toColumn;i--){
+						//if there is a player on any of the spaces
+						if(board[move.toRow][i].player()==
+								Player.BLACK||board[move.toRow]
+										[i].player()==Player.WHITE){
+							count++;
+						}
+						else{
+							
+						}
+					}
+					if(count==0){
+						return true;
+					}
+					else{
+						return false;
+					}
+				}
+				// if the player wanted to move right
+				else if(move.toColumn>move.fromColumn){
+					int count = 0;
+					for(int i=move.fromColumn+1;i<move.toColumn;i++){
+						//if there is a player on any of the spaces
+						if(board[move.toRow][i].player()==
+								Player.BLACK||board[move.toRow]
+										[i].player()==Player.WHITE){
+							count++;
+						}
+						else{
+							
+						}
+					}
+					if(count==0){
+						return true;
+					}
+					else{
+						return false;
+					}
+				}
+				else{
+					return false;
+				}
+
+			}
+			// if the selected move is in the same column
+			else if(move.fromColumn==move.toColumn){
+				
+				// if the player wants to move down
+				if(move.toRow>move.fromRow){
+					int count = 0;
+					for(int i=move.fromRow+1;i<move.toRow;i++){
+						//if there is a player on any of the spaces
+						if(board[move.toRow][i].player()==
+								Player.BLACK||board[move.toRow]
+										[i].player()==Player.WHITE){
+							count++;
+						}
+						else{
+							
+						}
+					}
+					if(count==0){
+						return true;
+					}
+					else{
+						return false;
+					}
+				}
+				//if the player wants to move up
+				else if(move.toRow<move.fromRow){
+					int count = 0;
+					for(int i=move.fromRow-1;i>move.toRow;i--){
+						//if there is a player on any of the spaces
+						if(board[move.toRow][i].player()==
+								Player.BLACK||board[move.toRow]
+										[i].player()==Player.WHITE){
+							count++;
+						}
+						else{
+							
+						}
+					}
+					if(count==0){
+						return true;
+					}
+					else{
+						return false;
+					}
+				}
+				else{
+					return false;
+				}
+			}
+			else{
+				return false;
+			}
 		}
 		else{
 			return false;
